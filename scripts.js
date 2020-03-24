04e7dcf7c01da1ce7113f964c669e13cda563a80
Rouzbeh Hatamy <roozbeh.hatamy@gmail.com>
2020-03-24 20:01:02 +0430

    add responsive menu button

--- scripts.js
+++ scripts.js
@@ -20,3 +20,21 @@ function myTimer() {
     var dt = new Date();
     document.getElementById("datetime").innerHTML = dt.toLocaleTimeString('fa-IR');
 }
+
+function plus() {
+    let rightSide = document.getElementsByClassName("rightHandSide")[0];
+    let icon = document.getElementById("plusIcon")
+    let visi = rightSide.style.display;
+    if (visi == "" || visi == "none") {
+        icon.style.transition = "1s"
+        icon.style.color = "rgb(98, 84, 229)"
+        icon.style.transform = "rotate(45deg)"
+        rightSide.style.display = "flex"
+    } else {
+        rightSide.style.display = "none"
+        icon.style.transition = "1s"
+        icon.style.color = "black"
+        icon.style.transform = "rotate(0deg)"
+    }
+
+}
